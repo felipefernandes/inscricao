e900619fe5a6b3b806bbedf694de0c076653af4b
Felipe Fernandes <ripcorp@gmail.com>
2013-07-31 12:44:12 -0300

    Revert "Versão 2.61"
    This reverts commit e7b471cd628bc54241fe16b884914f4a77907162.

--- relatorios/buscar.php
+++ relatorios/buscar.php
@@ -13,7 +13,7 @@ $total_registros = mysql_num_rows($consulta);
 ?>	
 <div class="container">
 	
-	<div class="well hidden-print">
+	<div class="well">
 		<h1>Inscrições <small>Relatórios (busca)</small></h1>
 	</div>	
 
